bdb0461adb0ca50abfef69c1ecbacccf430efe6e
xrosilloprofe <xv.rosillo@edu.gva.es>
2024-01-24 19:20:08 +0100

    20240124

--- src/main/java/Lista.java
+++ src/main/java/Lista.java
@@ -10,6 +10,11 @@ public class Lista {
         size=0;
     }
 
+    public Lista(int[] elements){
+        this();
+        addAll(elements);
+    }
+
     public int size(){return size;}
 
     public void addHead(int elem){
@@ -23,8 +28,104 @@ public class Lista {
     }
 
     public void addTail(int elem){
+        Nodo nodo=new Nodo(elem);
+        if(size==0){
+            head=nodo;
+        } else{
+            tail.setSiguiente(nodo);
 
+        }
+        tail=nodo;
+        size++;
     }
+
+    public Integer removeHead(){
+        if(head==null) return null;
+        int elem = head.getElem();
+        head=head.getSiguiente();
+        size--;
+
+        if(head==null) tail=null;
+        return elem;
+    }
+
+    public Integer removeTail(){
+        if(tail==null || head==null || size==0)
+            return null;
+        if(size==1 || head==tail)
+            return removeHead();
+        Nodo nodoBeforeTail = head;
+        while(nodoBeforeTail.getSiguiente()!=tail){
+            nodoBeforeTail=nodoBeforeTail.getSiguiente();
+        }
+        nodoBeforeTail.setSiguiente(null);
+
+        int elem=tail.getElem();
+        tail=nodoBeforeTail;
+
+        size--;
+        return elem;
+    }
+
+    public void clear(){
+        head=null;
+        tail=null;
+        size=0;
+    }
+
+    public boolean contains(int num){
+
+        boolean encontrado=false;
+        Nodo aux=head;
+
+        while(!encontrado && aux!=null){
+            if(aux.getElem()==num)
+                encontrado=true;
+            else
+                aux=aux.getSiguiente();
+        }
+        return encontrado;
+    }
+
+    public Integer get(int posicion){
+        if(posicion<0 || posicion>=size) return null;
+        Nodo aux=head;
+        for (int i = 0; i < posicion; i++) {
+            aux=aux.getSiguiente();
+        }
+        return aux.getElem();
+    }
+
+    public Integer remove(int posicion){
+        if(posicion<0 || posicion>=size) return null;
+        if(posicion==0) return removeHead();
+        if(posicion==size-1) return removeTail();
+
+        Nodo aux=head;
+        for (int i = 0; i < posicion-1; i++) {
+            aux=aux.getSiguiente();
+        }
+        int num = aux.getSiguiente().getElem();
+        aux.setSiguiente(aux.getSiguiente().getSiguiente());
+        size--;
+        return num;
+    }
+
+    public void addAll(int[] elements){
+        for(int element:elements)
+            addTail(element);
+    }
+
+    public int[] getAsArray(){
+        int[] array = new int[size];
+        Nodo aux=head;
+        for (int i = 0; i < size; i++) {
+            array[i]=aux.getElem();
+            aux=aux.getSiguiente();
+        }
+        return array;
+    }
+
 
 @Override
 public String toString(){
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Lista listaEnteros = new Lista();
+        listaEnteros.removeHead();
+        listaEnteros.removeTail();
         listaEnteros.addHead(5);
         listaEnteros.addHead(10);
         listaEnteros.addHead(15);
         System.out.println(listaEnteros);
+        listaEnteros.addTail(0);
+        listaEnteros.addTail(-5);
+        System.out.println(listaEnteros);
+//        listaEnteros.removeHead();
+//        listaEnteros.removeTail();
+//        System.out.println(listaEnteros);
+        System.out.println("5? -> " + listaEnteros.contains(5));
+        System.out.println("9? -> " + listaEnteros.contains(9));
+        System.out.println(listaEnteros.get(2));
+        System.out.println(listaEnteros.get(0));
+        System.out.println(listaEnteros.remove(2));
+        System.out.println(listaEnteros);
+        listaEnteros.addAll(new int[]{1,2,3,4,5});
+        System.out.println(listaEnteros);
+        System.out.println(Arrays.toString(listaEnteros.getAsArray()));
+
     }
 }
